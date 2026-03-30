eac3f9dd6b208198f4a3741c8345d04b19316617
PudimDeveloper <jonnas9908@gmail.com>
2026-03-30 16:11:05 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+#!/data/data/com.termux/files/usr/bin/env python3
+import os
+
+with open("/storage/emulated/0/soupmacro/comunicacao/name.txt",>
+        nome = f.read()
+
+comando = f'yt-dlp -o - "ytsearch1:{nome}" | mpv -'
+os.system(comando)
