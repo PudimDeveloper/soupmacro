e8c45777466a935d51f6484a6ef65517a506c52a
PudimDeveloper <jonnas9908@gmail.com>
2026-03-30 16:00:03 -0300

    Create install.bash

---
+++ install.bash
@@ -0,0 +1,10 @@
+pkg upgrade
+pkg update
+
+pkg install python -y
+pkg install yt-dlp -y
+pip install yt-dlp -y
+pkg install figlet -y
+
+echo "Finnaly" | figlet
+echo "2026 © PudimDev | DevWAve | ligHTDev"
