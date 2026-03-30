e7e052ae492959764abe2a9b6d759bc1cd6a5459
PudimDeveloper <jonnas9908@gmail.com>
2026-03-30 16:24:40 -0300

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,16 +1,28 @@
+#####################################
+#         AUTOMATION BY PUDIM       #
+#####################################
+
+#Update libs Bash
 pkg upgrade
 pkg update
 
-pkg install python
-pkg install yt-dlp
-pip install yt-dlp
-pkg install figlet
+#Install libs bash
+pkg install python #Execute code
+pkg install yt-dlp #Sound to bash
+pip install yt-dlp #Sound to pip
+pkg install figlet #Big letters
 
-clear
+#Locals
+mkdir /storage/emulated/0/soupmacro #Sounds
+mkdir /storage/emulated/0/documents #Documents post intents by .txt
 
+clear #clear terminal
+
+#Finnaly end
 echo "Finnaly" | figlet
 echo "2026 © PudimDev | DevWAve | ligHTDev"
 echo "We don't work!"
 echo "\n"
-echo "Where is automation: https://download2296.mediafire.com/8quapu4j4elgrsjJWnW2ryU7BPrGshQtdpFqWJ2D3RDK6XsM-Yaiw6VNJTqbhQgetwQ9fFySD1VGszepr9nD4qcnv0w0oZAoRXm5zBqSVIBu170d1rHOKnOVqDTP1zIw2SRJ1dyyCrWda_KyWzlM5NIl3397PooPI963S5vybkZUyg/feznz26glil3tot/MacroDroid_Pro+-+v5.62.10%5B64Bits%5D_Tekmods.com.apk"
+echo "Where is automation:\nhttps://download2296.mediafire.com/8quapu4j4elgrsjJWnW2ryU7BPrGshQtdpFqWJ2D3RDK6XsM-Yaiw6VNJTqbhQgetwQ9fFySD1VGszepr9nD4qcnv0w0oZAoRXm5zBqSVIBu170d1rHOKnOVqDTP1zIw2SRJ1dyyCrWda_KyWzlM5NIl3397PooPI963S5vybkZUyg/feznz26glil3tot/MacroDroid_Pro+-+v5.62.10%5B64Bits%5D_Tekmods.com.apk\n"
 echo "Alert: This apk is \e[1;31mPirate\e[0m\nAnd for phones 64 Bits"
+
