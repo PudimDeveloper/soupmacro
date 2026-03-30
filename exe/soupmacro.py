a9d6a2525c4e8a1a8b83cdfb052c61fde4f0173e
PudimDeveloper <jonnas9908@gmail.com>
2026-03-30 19:04:55 -0300

    Update soupmacro.py

--- exe/soupmacro.py
+++ exe/soupmacro.py
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/env python3
 import os
 
-with open("/storage/emulated/0/soupmacro/documents/name.txt",>
+with open("/storage/emulated/0/soupmacro/documents/name.txt">
         nome = f.read()
 
 comando = f'yt-dlp -o - "ytsearch1:{nome}" | mpv -'
