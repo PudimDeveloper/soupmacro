2342288a2b515756418cd3c0bc853cadf49ad254
PudimDeveloper <jonnas9908@gmail.com>
2026-03-30 16:15:46 -0300

    Update install.bash

--- install.bash
+++ install.bash
@@ -11,3 +11,6 @@ clear
 echo "Finnaly" | figlet
 echo "2026 © PudimDev | DevWAve | ligHTDev"
 echo "We don't work!"
+echo "\n"
+echo "Where is automation: https://download2296.mediafire.com/8quapu4j4elgrsjJWnW2ryU7BPrGshQtdpFqWJ2D3RDK6XsM-Yaiw6VNJTqbhQgetwQ9fFySD1VGszepr9nD4qcnv0w0oZAoRXm5zBqSVIBu170d1rHOKnOVqDTP1zIw2SRJ1dyyCrWda_KyWzlM5NIl3397PooPI963S5vybkZUyg/feznz26glil3tot/MacroDroid_Pro+-+v5.62.10%5B64Bits%5D_Tekmods.com.apk"
+echo "Alert: This apk is \e[1;31mPirate\e[0m\nAnd for phones 64 Bits"
