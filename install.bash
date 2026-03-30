8e592410f0610927bc8c37790d829f9f4e0e8ee1
PudimDeveloper <jonnas9908@gmail.com>
2026-03-30 17:56:43 -0300

    Update install.bash

--- install.bash
+++ install.bash
@@ -20,15 +20,15 @@ echo "..." > /storage/emulated/0/soupmacro/documents/name.txt
 clear #clear terminal
 
 #Finnaly end
-echo "Finnaly" | figlet
-echo "2026 © PudimDev | DevWAve | ligHTDev"
-echo "We don't work!"
-echo "\nTasker:"
-echo "\e[36mhttps://download2270.mediafire.com/2k5epdizy1ig1bX2ZpatD2OVDV1dqzgPrwZ9hWGDPQWcmTgLFxanU2_DHOfScjfNlaQjeEBIANR5PoutcoCcmkruRyEblXbDgiwHD21_n13VOb1ef4PisYQV_g4IJ8f7LrJvPsMs5Q3N97fA4BwISIu_b2TF9RGqi-9ZfBPUp6Hilw/vbgt3to8d1rllmx/Tasker+-+v6.6.20_Tekmods.com.apk\n\e[0m\n"
+echo -e "Finnaly" | figlet
+echo -e "2026 © PudimDev | DevWAve | ligHTDev"
+echo -e "We don't work!"
+echo -e "\nTasker:"
+echo -e "\e[36mhttps://download2270.mediafire.com/2k5epdizy1ig1bX2ZpatD2OVDV1dqzgPrwZ9hWGDPQWcmTgLFxanU2_DHOfScjfNlaQjeEBIANR5PoutcoCcmkruRyEblXbDgiwHD21_n13VOb1ef4PisYQV_g4IJ8f7LrJvPsMs5Q3N97fA4BwISIu_b2TF9RGqi-9ZfBPUp6Hilw/vbgt3to8d1rllmx/Tasker+-+v6.6.20_Tekmods.com.apk\n\e[0m\n"
 
-echo "Hotword Plugin: "
-echo "\e[36mhttps://play.google.com/store/apps/details?id=nl.jolanrensen.hotwordPluginFree\e[0m"
+echo -e "Hotword Plugin: "
+echo -e "\e[36mhttps://play.google.com/store/apps/details?id=nl.jolanrensen.hotwordPluginFree\e[0m"
 
 
 #Alert
-echo "\e[35mAlert: This apk is \e[1;31mPirate\e[0m\nAnd for phones 64 Bits\e[0m"
+echo -e "\e[35mAlert: This apk is \e[1;31mPirate\e[0m\nAnd for phones 64 Bits\e[0m"
